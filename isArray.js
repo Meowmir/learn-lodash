f4f1438c287c625465788bed99630925d2e79cae
Meowmir <disewji@gmail.com>
2023-06-20 14:32:05 +0200

    Feature isArray

---
+++ isArray.js
@@ -0,0 +1,8 @@
+/**
+ @param {array} valueToCheck - The array to check if it's an array.
+ @returns {boolean} - Returns true is value is array.
+ */
+
+export default function isArray(valueToCheck) {
+  return Array.isArray(valueToCheck)
+}
